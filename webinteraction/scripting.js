1db1584d9598aece0aac413e0f3044be6a49983c
dragonilly <134251470+JikokuAice@users.noreply.github.com>
2023-08-31 15:04:47 +0545

    adding carousel scroll in Web intercation
    using simple js and <ul> tag

---
+++ webinteraction/scripting.js
@@ -0,0 +1,18 @@
+
+
+    const curid = document.querySelector('#curid');
+    const ul = curid.querySelector('ul');
+    const leftButton = curid.querySelector('.arrow1');
+    const rightButton = curid.querySelector('.arrow2');
+
+  
+
+    // Add click event listener to the left button
+    leftButton.addEventListener('click', () => {
+        ul.scrollLeft -= 150;
+    });
+
+    // Add click event listener to the right button
+    rightButton.addEventListener('click', () => {
+        ul.scrollLeft += 150;
+    });
